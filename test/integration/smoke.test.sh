f5ba0cc2cd48361ce51b17340c2aaeff4f113868
René Föhring <rene.foehring@5minds.de>
2020-03-12 08:14:58 +0100

    Add help text output to smoke test

--- test/integration/smoke.test.sh
+++ test/integration/smoke.test.sh
@@ -18,16 +18,32 @@ cd $PROJECT_ROOT
 # execution
 
 echo ""
+echo "node dist/atlas.js login --help"
+node dist/atlas.js login --help
+
+echo ""
 echo "node dist/atlas.js login http://localhost:8000 --root --output json"
 node dist/atlas.js login http://localhost:8000 --root --output json
+
+echo ""
+echo "node dist/atlas.js session-status --help"
+node dist/atlas.js session-status --help
 
 echo ""
 echo "node dist/atlas.js session-status --output json"
 node dist/atlas.js session-status --output json
 
 echo ""
+echo "node dist/atlas.js deploy-files --help"
+node dist/atlas.js deploy-files --help
+
+echo ""
 echo "node dist/atlas.js deploy-files fixtures/wait-demo.bpmn --output json"
 node dist/atlas.js deploy-files fixtures/wait-demo.bpmn --output json
+
+echo ""
+echo "node dist/atlas.js start-process-model --help"
+node dist/atlas.js start-process-model --help
 
 echo ""
 echo "node dist/atlas.js start-process-model wait_demo StartEvent_1 --input-values '{"seconds": 1}' --output json | ..."
@@ -36,16 +52,32 @@ node dist/atlas.js start-process-model wait_demo StartEvent_1 --input-values '{"
                   | node dist/atlas.js show-process-instance
 
 echo ""
+echo "node dist/atlas.js list-process-instances --help"
+node dist/atlas.js list-process-instances --help
+
+echo ""
 echo "node dist/atlas.js list-process-instances"
 node dist/atlas.js list-process-instances
+
+echo ""
+echo "node dist/atlas.js list-process-models --help"
+node dist/atlas.js list-process-models --help
 
 echo ""
 echo "node dist/atlas.js list-process-models"
 node dist/atlas.js list-process-models
 
 echo ""
+echo "node dist/atlas.js remove --help"
+node dist/atlas.js remove --help
+
+echo ""
 echo "node dist/atlas.js remove wait_demo --yes"
 node dist/atlas.js remove wait_demo --yes
+
+echo ""
+echo "node dist/atlas.js logout --help"
+node dist/atlas.js logout --help
 
 echo ""
 echo "node dist/atlas.js logout"
