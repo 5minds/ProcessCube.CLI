5b1232a2cca39e84cb534c6fe4aa87b29f93f4e4
René Föhring <rene.foehring@5minds.de>
2020-04-08 14:22:12 +0200

    Improve error message for unreachable authority

--- src/commands/login/login.ts
+++ src/commands/login/login.ts
@@ -9,11 +9,12 @@ import {
 } from '../../session/atlas_session';
 
 import { isUrlAvailable } from '../../client/is_url_available';
-import { logError, logWarning } from '../../cli/logging';
+import { logError, logWarning, removeMultilineIndent } from '../../cli/logging';
 import { startServerToLoginAndWaitForAccessTokenFromIdentityServer } from './express_server';
 
 const ONE_YEAR_IN_MILLISECONDS = 365 * 86400 * 1000;
 const ANONYMOUS_TOKEN_LIFETIME_IN_MILLISECONDS = 99 * ONE_YEAR_IN_MILLISECONDS;
+const DEFAULT_IDENTITY_SERVER_URL = 'http://localhost:5000/';
 
 export async function login(
   givenEngineUrl: string,
@@ -33,7 +34,11 @@ export async function login(
     logWarning(chalk.yellowBright(`Using engine url from previous session: ${engineUrl}`));
   }
 
-  if (engineUrl.match(/:\/\//) == null) {
+  engineUrl = engineUrl.replace(/\/$/, '');
+
+  const isMissingSchemeSeparator = engineUrl.match(/:\/\//) == null;
+
+  if (isMissingSchemeSeparator) {
     const isAvailableViaHttps = await isUrlAvailable(`https://${engineUrl}`);
     if (isAvailableViaHttps) {
       engineUrl = `https://${engineUrl}`;
@@ -57,7 +62,7 @@ export async function login(
     newSession = await loginViaIdentityServer(engineUrl);
 
     if (newSession == null) {
-      logError(`Could not connect to ${engineUrl}`);
+      logError(`Could not connect to engine: ${engineUrl}`);
       process.exit(1);
     }
   }
@@ -87,6 +92,27 @@ async function loginViaIdentityServer(engineUrl: string): Promise<AtlasSession |
   const identityServerUrl = await getIdentityServerUrlForEngine(engineUrl);
   if (identityServerUrl == null) {
     return null;
+  }
+
+  const identityServerIsAvailable = await isUrlAvailable(identityServerUrl);
+  if (identityServerIsAvailable === false) {
+    logError(`The engine returned this unreachable authority URL: ${identityServerUrl}`);
+    console.warn('');
+    if (identityServerUrl === DEFAULT_IDENTITY_SERVER_URL) {
+      console.warn(
+        chalk.redBright.bold(`If you're in a development setting, your engine might also allow anonymous root access:`)
+      );
+      console.warn('');
+      console.warn(chalk.redBright.bold(`  $ atlas login ${engineUrl} --root`));
+      console.warn('');
+      console.warn(
+        chalk.redBright.bold(`If you're in a production setting, you should avoid enabling anonymous root access.`)
+      );
+    } else {
+      console.warn(chalk.redBright.bold(`To be able to login, please ensure the authority is reachable.`));
+    }
+
+    process.exit(1);
   }
 
   const { accessToken, idToken, expiresAt } = await startServerToLoginAndWaitForAccessTokenFromIdentityServer(
