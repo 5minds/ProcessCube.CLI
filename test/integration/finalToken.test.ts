3cd6d10bf19bb37afb293851c5972b0a4374e11d
betueldemirtas90 <62238043+betueldemirtas90@users.noreply.github.com>
2021-03-31 10:03:23 +0200

    Apply suggestions from code review
    Co-authored-by: Sebastian Griesa <sebastian.griesa@gmail.com>

--- test/integration/finalToken.test.ts
+++ test/integration/finalToken.test.ts
@@ -9,22 +9,19 @@ describe('atlas', () => {
 
    execAsJson('deploy-files fixtures/finalTokenTest.bpmn');
 
-   const result = execAsJson('start-process-model finalTokenTest StartEvent_1mox3jl --input-values \'{"seconds": 1}\' --wait');
+   const result = execAsJson('start-process-model finalTokenTest StartEvent_1mox3jl --wait');
 
-   //const processInstanceId = result?.result[0]?.processInstanceId;
 
    const correlationId = result?.result[0]?.correlationId;
-   const filterByCorrelationId = execAsJson(`list-process-instances --filter-by-correlation-id ${correlationId}`);
+   const processInstances = execAsJson(`list-process-instances --filter-by-correlation-id ${correlationId}`);
 
    const currentToken = filterByCorrelationId?.result[0]?.finalToken;
 
    const expectedToken = '{"myTestValue":"test"}';
-   assert.ok(currentToken, expectedToken,);
+   assert.deepEqual(currentToken, expectedToken);
 
    execAsText('logout');
 
-   const session = execAsJson('session-status');
-   assert.equal(session.accessToken, null);
 
  });
 });
