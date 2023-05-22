51a765be5d4eb8d05a716db4bf1905a6b9c6e922
samuelsaengerboeger <114991955+samuelsaengerboeger@users.noreply.github.com>
2023-05-22 13:28:09 +0200

    change Import

--- src/client/api_client.ts
+++ src/client/api_client.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 
 import { DataModels as AtlasEngineDataModels, EngineClient } from '@atlas-engine/atlas_engine_client';
-import { FlowNodeInstanceState } from '@atlas-engine/atlas_engine_sdk/dist/DataModels/FlowNodeInstance';
+import { FlowNodeInstanceState } from '@atlas-engine/atlas_engine_sdk/';
 
 import { getIdentity } from './identity';
 
