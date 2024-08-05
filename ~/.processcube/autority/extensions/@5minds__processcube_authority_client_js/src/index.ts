10e4d9ac0fe2f3c5f377f4ccc84434bbdb540359
Sebastian Wehmeyer <sebastianwehmeyer@Sebastians-MacBook-Pro.local>
2024-08-05 14:24:01 +0200

    Initial commit

--- ~/.processcube/autority/extensions/@5minds__processcube_authority_client_js/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./authorityClient";
-export * from "./errors";
-export * from "./models";
-export * from "./storage";
-export * from "./transaction-manager";
-export * from "./utils";
