f3218e33004e30e6cc88d00c4b7117d350fec7f9
Sebastian Wehmeyer <sebastianwehmeyer@Sebastians-MacBook-Pro.local>
2024-08-22 11:43:47 +0200

    remove stable

--- src/commands/install-extension/index.ts
+++ src/commands/install-extension/index.ts
@@ -41,12 +41,6 @@ export async function onLoad(cli: CLI): Promise<void> {
           type: 'boolean',
           default: false,
         },
-        {
-          name: 'stable',
-          description: 'Install extension to studio too if --insiders is used',
-          type: 'boolean',
-          default: false,
-        },
       ],
     },
     runCommand,
