cbe33ac6c8c544648ef94e3b232f93d1912d13c8
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2025-10-03 13:21:22 +0200

    Add build to a single binary file

--- src/yargs.ts
+++ src/yargs.ts
@@ -7,8 +7,7 @@ const SCRIPT_NAME = 'pc';
 const VERSION = require('../package.json').version;
 
 export async function useYargsForCommandLineInterface(cli: CommandLineInterface): Promise<void> {
-  const yargs = require('yargs');
-  const program = yargs(process.argv.slice(2));
+  const program = require('yargs');
 
   program
     .version(VERSION)
